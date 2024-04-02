062eb9e573af4bdd3cc21045b69e0f13cf28e68f
balbii <93939187+balbii@users.noreply.github.com>
2024-04-02 17:56:18 -0300

    Corrigindo ultimo commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,6 @@ const App = () => {
       </div>
     );
   }
-  const tarefas = [
-    { title: 'Tirar o lixo', id: 1 },
-    { title: 'Lavar o banheiro', id: 2 },
-    { title: 'Levar o cachorro pra passear', id: 3 },
-  ];
-  
-  export default function ListaTarefas() {
-    const listItems = tarefas.map(tarefa =>
-      <li
-        key={tarefa.id}
-      >
-        {tarefa.title}
-      </li>
-       );
-
-       return (
-         <ul>{listItems}</ul>
-       );
-     }
 
   export default App;
   
