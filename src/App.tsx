3999d42479fc92c3a33ad0bdf2308818f54c462d
balbii <93939187+balbii@users.noreply.github.com>
2024-04-02 18:38:00 -0300

    adiconei lista

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,21 @@ const App = () => {
       <div>
         <h1>Bem vindo ao mundo React</h1>
         <button>eu sou um botão html</button>
+        <ul>{listTarefas}</ul>
       </div>
     );
   }
+  const tarefas = [
+    { title: 'Tirar o lixo', id: 1 },
+    { title: 'Lavar o banheiro', id: 2 },
+    { title: 'Levar o cachorro pra passear', id: 3 },
+  ];
+  
+  const listTarefas = tarefas.map(tarefa =>
+    <li key={tarefa.id}>
+      {tarefa.title}
+    </li>
+      );
 
   export default App;
   
